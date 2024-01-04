f3fa3ae282ae606ae3d5166b9a770a3f9d2cf1de
aidan <aidanschneider@telus.net>
2024-01-04 12:08:18 -0800

    Foundation for pipeline.

---
+++ Aquarium-Detection-1/README.dataset.txt
@@ -0,0 +1,6 @@
+# Aquarium Detection > 2023-12-30 5:08pm
+https://universe.roboflow.com/object-detection-bounding-box-gpn4e/aquarium-detection-pjw4z
+
+Provided by a Roboflow user
+License: CC BY 4.0
+
--- cvmodel.py
+++ cvmodel.py
@@ -1,1 +1,13 @@
 # beginning of cv model
+import fiftyone as fo
+import fiftyone.zoo as foz
+
+dataset = foz.load_zoo_dataset("quickstart")
+
+# collects from the user/fiftyone/... file path - run through command line, doesn't like .ipynb
+
+session = fo.launch_app(dataset, port=5151)
+session.dataset = dataset
+
+
+session.wait()
